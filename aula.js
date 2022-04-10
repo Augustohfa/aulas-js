7577a88e56e1ed1787235ecda5b27853fb079c6a
Augustohfa <86023411+Augustohfa@users.noreply.github.com>
2022-04-10 10:34:12 -0300

    Update aula.js

--- aula.js
+++ aula.js
@@ -434,7 +434,7 @@
 // }
 // delete mouse.velocidade;
 
-//Exemplos de que os objetos tem natureza dinâmica no JS, ou seja, podem ser modificados facilmente
+// Exemplos de que os objetos tem natureza dinâmica no JS, ou seja, podem ser modificados facilmente
 
 // const celular = {
 //     marcaCelular : 'Samsung',
@@ -458,28 +458,306 @@
 // const objeto2 = {...celular};//Spread ou 3 pontos
 // console.log(objeto2)
 
-//Math methods (pesquisar google JavaScript math)
+// Math methods (pesquisar google JavaScript math)
 
 // let numeroAleatorio = Math.randon()
 // console.log(Math.round (numeroAleatorio))
 
 // Math.max(1,3,4,5)//Retorna o maior valor
-//Math.min(1,4,4,2)//Retorna o menor número
+// Math.min(1,4,4,2)//Retorna o menor número
 
-//Tipo primitivo
-    // const mensagem = 'Blablabla mensagem';
+// Tipo primitivo
+//     const mensagem = 'Blablabla mensagem';
 
-//Tipo objeto
+// Tipo objeto
 
-    // const outraMensagem = new String('Bom dia!');
+//     const outraMensagem = new String('Bom dia!');
 
 // mensagem = string
 
 // outraMensagem = objeto
 
-//objeto.length - retorna o tamanho da string
+// objeto.length - retorna o tamanho da string
 // const outraMensagem = new String('Bom dia!');
 // console.log(outraMensagem[2])// Retorna 'm' pois é a letra que está nessa posição
 // console.log(outraMensagem.includes('Bom'))//Retorna True pois existe o termo 'bom' no objeto
 // console.log(outraMensagem.includes('Ababa'))//Retorna false pois não existe esse termo no objeto
-// console.log(outraMensagem.startsWith('b'))
+// console.log(outraMensagem.startsWith('b'))\
+
+
+//Template Literal
+
+// const message = 
+// "Hi this is my\n first message!";
+
+//Object {}
+//Boolean: True or False
+//String '', ""
+//Template ``
+// const anotherMessage = `Teste, essa mensagem é pra ser 'literalmente'os
+// espaços e todo resto`
+
+// console.log(anotherMessage)
+
+// //Exemplo
+// let elo = "Platina 2"
+// let user = 'Guutin'
+//     const email = 
+// `Olá ` + user + ` seu atual rank é 
+// ` + elo + ``;
+// console.log(email)
+
+
+// const nome = 'Augusto';
+
+
+
+// const eloVal = {
+//     rank: 'ouro',
+//     divisao: 2
+// };
+
+// const email = 
+// `Olá ${nome}
+
+// Seu atual elo é ${eloVal.rank+ ' ' + eloVal.divisao};
+
+// Para ver o treinamento específico do seu rank acesse ${treinoElo(eloVal.rank)}.
+
+// Obrigado
+// Guutin`;
+
+// function treinoElo(elo){
+//     for(rank in elo){
+//         if (elo[rank] = 'platina'){
+//             return'treinoplatina.com'
+//         }else if(elo[rank] = 'ouro'){
+//             return 'treinoouro.com'
+//         }else if(elo[rank] = 'diamante'){
+//             return 'treinodiamante.com'
+//         }else if(elo[rank] = 'prata'){
+//             return 'treinoprata.com'
+//         }else if(elo[rank] = 'bronze'){
+//             return 'treinobronze.com'
+//         }
+        
+
+//     }
+// }
+// console.log(email)
+
+
+
+
+
+
+
+
+
+
+
+//   const filme = {
+//          titulo: 'Vingadores',
+//          ano: 2018,
+//          direto: 'Augusto',
+//          personagem: 'Miranha'
+//      }
+//      function exibirPropriedades(obj){
+//         for(prop in obj){
+//             if(typeof obj[prop] === 'string')
+//                  console.log(prop, obj[prop])
+//          }
+//       }
+//       exibirPropriedades(filme)
+
+// const data1 = new Date();
+// console.log(data1)
+
+// const data2 = new Date('March 31 2022 09:27')
+// console.log(data2)
+//
+//Criar endereço
+//Rua
+//Cidade
+// CEP
+//funcao exibirEndereço
+
+// let = endereco = {
+//     rua: 'Servidão Mercedes Maria Luiza',
+//     cidade: 'Florianópolis',
+//     cep: '88058-405'
+// };
+
+// function exibirEndereco(endereco){
+//     for (let chave in endereco){
+//         console.log(chave,endereco[chave]);
+//     }
+// }
+
+// exibirEndereco(endereco);
+
+// Igualdade de objetos
+
+// function Endereco(rua,cidade,cep){
+//     this.rua = rua,
+//     this.cidade = cidade,
+//     this.cep = cep
+// }
+
+// const endereco1 = new Endereco('a', 'b','c');
+// const endereco2 = new Endereco('a', 'b', 'c');
+
+// // function saoIguais(endereco1, endereco2){
+// //     //comparar se as propriedades são iguais
+// //     return endereco1.rua == endereco2.rua &&
+// //     endereco1.cidade == endereco2.cidade &&
+// //     endereco1.cep == endereco2.cep
+// // }
+
+// // console.log(saoIguais(endereco1, endereco2));
+
+// function temEnderecoDeMemoriaIguais(endereco1, endereco2){
+//     return endereco1 == endereco2
+// }
+
+// console.log(temEnderecoDeMemoriaIguais(endereco1, endereco2))
+
+/* Criar um objeto de postagem que vai conter as seguintes propriedades
+
+postagem
+
+titulo
+mensagem
+autor
+vizualizações
+comentarios (autor, mensagem)*/
+
+
+// let postagem = {
+//     titulo: 'a',
+//     mensagem: 'b',
+//     autor: 'c',
+//     visualizacoes: '1',
+//     comentarios: [
+//         {autor: 'a', mensagem: 'b'},
+//         {autor: 'a', mensagem: 'a'}
+//     ]
+// }
+
+// console.log(postagem.mensagem)
+
+//Criar um objeto postagem
+//Titulo, objeto, autos, visualizacoes, arrey de comentarios, estaaovivo
+
+// function criarPostagem(titulo, mensagem, autor){
+//         this.titulo = titulo,
+//         this.mensagem = mensagem,
+//         this.autor = autor,
+//         this.live = live
+// }
+
+
+//     postagem = new criarPostagem('Era uma vez uma menina', 'Sei la uma mensagem qualquer', 'Guutin')
+//     console.log(postagem)
+
+
+//Faixa de preço
+
+    //Primeira opção
+
+    // let faixas = [
+    //     {tooltip: 'Até R$700', minimo: 0, máximo: 700}, 
+    //     {tooltip: 'Até R$1000', minimo: 700, máximo: 1000},
+    //     {tooltip: 'Acima de R$1000', minimo: 1000, máximo: 9999999999}
+     
+
+    // ];
+
+    // //Segunda opção (Factory Function)
+    // function criarFaixaPreco(tooltip, minimo, maximo){
+    //     return{
+    //         tooltip,
+    //         minimo,
+    //         maximo
+    //     }
+    // }
+
+    // let faixas2 = [
+    //     criarFaixaPreco('Até R$700',0, 700),
+    //     criarFaixaPreco('Até R$1000', 700,1000),
+    //     criarFaixaPreco('Acima de 1000', 1000, 9999999)
+    // ];
+
+    // console.log(faixas)
+    // console.log(faixas2);
+
+    //Terceira opção (Constructive Functions)
+
+
+    // function FaixaPreco(tooltip,minimo,maximo){
+    //     this.tooltip = tooltip,
+    //     this.minimo = minimo,
+    //     this.maximo = maximo
+    // }
+
+    // let faixas3 = [
+    //     new FaixaPreco('Até R$700', 0, 700),
+    //     new FaixaPreco('Até R$1000', 700, 1000),
+    //     new FaixaPreco('Acima de 1000', 1000, 999999999)
+    // ];
+
+    // console.log(faixas3[2].tooltip)
+
+    //JS Introdução a arrays
+    //Add novos elementos
+    //Encontrar Elementos
+    //Remover Elementos
+    //Dividir Elementos
+    //Dividir Arrays
+    //Combinar Arrays
+
+    // const numeros = [1,2,3];
+    // //Inicio
+    // // numeros.unshift(0); //.unshift insere o valor no array na esquerda de todos os números
+    // // console.log(numeros)
+    // //Meio
+    // numeros.splice(1,0,'b') //.Splice insere o valor no array no lugar escolhido, primeiro numero = posicao, segundo numero 0 = nao deleta, 1= deleta, terceiro numeor = valor
+    // console.log(numeros)
+    // //Fim
+    // numeros.push(5);// .push Mesma coisa que .unshift porém ao contrário, a invéz de colocar no começo, colcoa no final
+    // console.log(numeros);
+
+    // const teste = [];
+    // teste.push('teste1')
+    // console.log(teste)
+
+    // const numeros = [1,2,3,2,2,2,2,2,2,2,22,,22,2,2,4];
+    // console.log(numeros.indexOf(2)); //.indexOf mostra o indice em que o número indicado está.
+    // numeros.lastIndexOf(2); //Mesma coisa que o indexOf, porém mostra somente o ultimo.
+
+    // const numeros = [1,2,3,1,4,5,3]
+   
+    // console.log(numeros.indexOf(2) !== -1);
+
+    // console.log(numeros.includes(2))
+
+    // Encontrando elementos(tipos de referencia)
+
+        // const marcas = [
+        //     {id: 1, nome: 'Sexo é ruim'},
+        //     {id: 2, nome: 'Sexo é bom'},
+        //     {id: 3, nome: 'Sexo é mais ou menos'}
+        // ]
+
+        // const marca = marcas.find(marca => marca.nome == 'Sexo é ruim');
+
+        // console.log(marca);
+
+        //Arrow Function https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Functions/Arrow_functions.\
+
+    // const marcas = [
+    //     {id:1, nome:'Teste1'},
+    //     {id:2, nome:'Teste2'},
+    //     {id:3, nome:'Teste3'},
+    // ];
+
